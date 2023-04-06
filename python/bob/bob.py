b953c0612daf1b49a6e8fb696c2b23125192860d
Tyriek Barker <tyriekbarker@gmail.com>
2023-04-06 10:41:59 -0400

    Update Bob exercise

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -5,25 +5,24 @@
 # import re, check if hey_bob matches each regular expression and return fixed reponse based on this
 # If SENTENCE ||does not match|| (regular expression), evaluate against next (regular expression)...
 # ...until there is a match. If no match for given expressions, return "Whatever."
+# ALWAYS have a thorough understanding of the end result before writing your code
 import re
 
 
 def response(hey_bob):
-    sure = re.compile("[A-Za-z\"-// ]*[?]$")
+    sure = re.compile("[A-Za-z\"-// q ]*[?]$")
 
     chill_out = re.compile("^[A-Z\"-// ]+$")
     print(chill_out.match(hey_bob))
     calm_down = re.compile("[A-Z\"-// ]*[?]$")
 
-    fine = re.compile("^$")
-
-    if sure.match(hey_bob):
+    if not hey_bob.strip():
+        return "Fine, be that way!"
+    elif sure.match(hey_bob):
         return "Sure."
     elif chill_out.match(hey_bob):
         return "Whoa, chill out!"
     elif calm_down.match(hey_bob):
         return "Calm down, I know what I'm doing!"
-    elif fine.match(hey_bob):
-        return "Fine. Be that way!"
     else:
         return "Whatever."
